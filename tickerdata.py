0a12daed5bfd33217046aae66d295bdfaf0e5d80
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-02-10 20:40:45 -0500

    Getting data on tickers/getting tickers

--- tickerdata.py
+++ tickerdata.py
@@ -14,38 +14,43 @@ style.use('ggplot')
 if not os.path.exists('TickerData'):
     os.makedirs('TickerData')
 
-with open("sp500tickers.pickle","rb") as f:
-    tickers = pickle.load(f)
+# with open("sp500tickers.pickle","rb") as f:
+#     tickers = pickle.load(f)
 
 tickers = []
-for i in os.listdir('TickerData'):
-    tickers.append(i[:-4])
+# for i in os.listdir('TickerData'):
+#     tickers.append(i[:-4])
+
+with open('russell3000.csv', 'r') as f:
+  reader = csv.reader(f)
+  for i in reader:
+      tickers.append(i[0])
 
 start = dt.datetime(2002,1,1)
 end = dt.datetime(2017,12,31)
 
-# print(len(tickers))
-# fails = 0
-# for ticker in tickers:
-#     print(ticker)
-#     if os.path.isfile('{}.csv'.format(ticker)):
-#         continue
-#
-#     try:
-#         df = web.DataReader(ticker, 'google', start, end)
-#         # df = df.reset_index(['Symbol'])           used with morningstar
-#         # df = df.drop(['Symbol'], axis = 1)
-#         #
-#         # df = df.reset_index(['Date'])
-#         # df['Date'] = pd.to_datetime(df['Date'])
-#         # df.index = df['Date']
-#         # df = df.drop(['Date'], axis = 1)
-#
-#         df.to_csv('TickerData/{}.csv'.format(ticker))
-#         time.sleep(2)
-#     except:
-#         print('fail')
-#         fails += 1
+print(len(tickers))
+fails = 0
+for ticker in tickers:
+    print(ticker)
+    if os.path.isfile('TickerData/{}.csv'.format(ticker)):
+        continue
+
+    try:
+        df = web.DataReader(ticker, 'google', start, end)
+        # df = df.reset_index(['Symbol'])           used with morningstar
+        # df = df.drop(['Symbol'], axis = 1)
+        #
+        # df = df.reset_index(['Date'])
+        # df['Date'] = pd.to_datetime(df['Date'])
+        # df.index = df['Date']
+        # df = df.drop(['Date'], axis = 1)
+
+        df.to_csv('TickerData/{}.csv'.format(ticker))
+        time.sleep(2)
+    except:
+        print('fail')
+        fails += 1
 
 # print(fails)
 
