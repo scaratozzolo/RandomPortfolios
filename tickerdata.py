885c102b15c451ba8586928de551fdf939c37d0e
Scott Caratozzolo <scaratozzolo12@gmail.com>
2018-02-11 20:52:40 -0500

    updates

--- tickerdata.py
+++ tickerdata.py
@@ -26,34 +26,37 @@ with open('russell3000.csv', 'r') as f:
   for i in reader:
       tickers.append(i[0])
 
-start = dt.datetime(2002,1,1)
+start = dt.datetime(1987,1,1)
 end = dt.datetime(2017,12,31)
-
-print(len(tickers))
-fails = 0
-for ticker in tickers:
-    print(ticker)
-    if os.path.isfile('TickerData/{}.csv'.format(ticker)):
-        continue
-
-    try:
-        df = web.DataReader(ticker, 'google', start, end)
-        # df = df.reset_index(['Symbol'])           used with morningstar
-        # df = df.drop(['Symbol'], axis = 1)
-        #
-        # df = df.reset_index(['Date'])
-        # df['Date'] = pd.to_datetime(df['Date'])
-        # df.index = df['Date']
-        # df = df.drop(['Date'], axis = 1)
-
-        df.to_csv('TickerData/{}.csv'.format(ticker))
-        time.sleep(2)
-    except:
-        print('fail')
-        fails += 1
-
-# print(fails)
 
 
 df = web.DataReader('SPY', 'google', start, end)
 df.to_csv('TickerData/SP500.csv')
+
+print(len(tickers))
+fails = 0
+running = True
+while running:
+    for ticker in tickers:
+        print(ticker)
+        if os.path.isfile('TickerData/{}.csv'.format(ticker)):
+            continue
+
+        try:
+            df = web.DataReader(ticker, 'google', start, end)
+            # df = df.reset_index(['Symbol'])           used with morningstar
+            # df = df.drop(['Symbol'], axis = 1)
+            #
+            # df = df.reset_index(['Date'])
+            # df['Date'] = pd.to_datetime(df['Date'])
+            # df.index = df['Date']
+            # df = df.drop(['Date'], axis = 1)
+
+            df.to_csv('TickerData/{}.csv'.format(ticker))
+        except:
+            print('fail')
+            fails += 1
+
+    if len(os.listdir('TickerData')) > 2970:
+        running = False
+# print(fails)
